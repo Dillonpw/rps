199012582b6305d1b69c2435038c99bb59cc90f7
dillonpw <dillonpwalsh10@gmail.com>
2023-07-28 15:33:58 -0400

    added win check/ formatted with prettier

--- app.js
+++ app.js
@@ -1,30 +1,45 @@
-const choices = ['rock', 'paper', 'scissors']
-const playerScore = ""
-const computerScore = ""
-const winnerScore = 5
+const choices = ["rock", "paper", "scissors"];
+const playerScore = "";
+const computerScore = "";
+const winnerScore = 5;
 
-function getComputerChoice(){
-    const randomChoice = Math.floor(Math.random() * choices.length);
-    let computerPick = choices[randomChoice].toLowerCase();
-    return choices[computerPick];
+function getComputerChoice() {
+  const randomChoice = Math.floor(Math.random() * choices.length);
+  let computerPick = choices[randomChoice].toLowerCase();
+  return choices[computerPick];
 }
 
-function play(getUserChoice){
+function play(getUserChoice) {
+  while (playerScore || computerScore < winnerScore) {
     const computerPlay = getComputerChoice();
-    let result = '';
+    let result = "";
 
-    if (getUserChoice === computerPlay){
-        result = "It's a Tie!"
-    } else if (getUserChoice === 'rock' && computerPlay ==='paper'
-    || getUserChoice === 'paper' && computerPlay === 'scissors' 
-    || getUserChoice === 'scissors' && computerPlay === 'rock'){
-    return "Computer wins!", computerScore += 1
-
-}else if (getUserChoice === 'rock' && computerPlay === 'scissors' 
-    || getUserChoice === 'scissors' && computerPlay === 'paper'
-    || getUserChoice === 'paper' && computerPlay === 'rock'){
-    return "You Win!", playerScore +=1
+    if (getUserChoice === computerPlay) {
+      result = "It's a Tie!";
+    } else if (
+      (getUserChoice === "rock" && computerPlay === "paper") ||
+      (getUserChoice === "paper" && computerPlay === "scissors") ||
+      (getUserChoice === "scissors" && computerPlay === "rock")
+    ) {
+      return "Computer wins!", (computerScore += 1);
+    } else if (
+      (getUserChoice === "rock" && computerPlay === "scissors") ||
+      (getUserChoice === "scissors" && computerPlay === "paper") ||
+      (getUserChoice === "paper" && computerPlay === "rock")
+    ) {
+      return "You Win!", (playerScore += 1);
+    }
+    alert(
+      "You chose: " +
+        { getUserChoice } +
+        "\nComputer Chose: " +
+        { computerPlay } +
+        "\n\n" +
+        { result } +
+        "\n" +
+        { playerScore } +
+        "\n" +
+        { computerScore }
+    );
+  }
 }
-alert('You chose: ${getUserChoice}\nComputer Chose: ${computerPlay}\n\n${result}\n${playerScore}\n${computerScore')
-}
-
