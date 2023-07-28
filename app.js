af3e1552851999b2c008064a74689338c71c98f4
dillonpw <dillonpwalsh10@gmail.com>
2023-07-28 11:33:05 -0400

    round function added

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const choices = ['rock', 'paper', 'scissors']
 
-function computerChoice(){
-    const randomChoice = Math.floor(Math.random() * choices.length);
+function getComputerChoice(){
+    const randomChoice = Math.floor(Math.random() * choices.length).toLowerCase;
     let computerPick = choices[randomChoice]
+    console.log(computerPick)
 }
-console.log(computerPick)
+
+
+function rpsRound(){
+    let playerPick = prompt("Choose your weapon: ").toLowerCase
+    getComputerChoice()
+    if (playerPick == computerPick){
+    return "It's a tie!"
+}   else if (playerPick == 'rock' && computerPick == 'paper'
+|| playerPick == 'paper' && computerPick == 'scissors' 
+|| playerPick == 'scissors' && computerPick == 'rock'){
+    return "Computer wins!"
+}else if (playerPick == 'rock' && computerPick == 'scissors' 
+|| playerPick == 'scissors' && computerPick == 'paper'
+|| playerPick == 'paper' && computerPick == 'rock'){
+    return "You Win!"
+}
+}
+rpsRound()
