b7735005eca3a3ceaa19998cc195e0648c53df64
dillonpw <dillonpwalsh10@gmail.com>
2023-07-28 13:28:32 -0400

    added HTML buttons/ hopefully working prog

--- app.js
+++ app.js
@@ -6,44 +6,24 @@ const winnerScore = 5
 function getComputerChoice(){
     const randomChoice = Math.floor(Math.random() * choices.length);
     let computerPick = choices[randomChoice].toLowerCase();
-    return computerPick;
+    return choices[computerPick];
 }
 
-function getPlayerChoice(){
-    let playerPick = prompt("Choose rock, paper, or scissors: ");
-    playerPick = playerPick.toLowerCase();
-    return playerPick;
+function play(getUserChoice){
+    const computerPlay = getComputerChoice();
+    let result = '';
+
+    if (getUserChoice === computerPlay){
+        result = "It's a Tie!"
+    } else if (getUserChoice === 'rock' && computerPlay ==='paper'
+    || getUserChoice === 'paper' && computerPlay === 'scissors' 
+    || getUserChoice === 'scissors' && computerPlay === 'rock'){
+    return "Computer wins!", computerScore += 1
+
+}else if (getUserChoice === 'rock' && computerPlay === 'scissors' 
+    || getUserChoice === 'scissors' && computerPlay === 'paper'
+    || getUserChoice === 'paper' && computerPlay === 'rock'){
+    return "You Win!", playerScore +=1
 }
-function checkWinner(){
-    if (playerScore == winnerScore){
-        return "You won 5 rounds! Game over"
-    }else if (computerScore == winnerScore){
-        return "Computer won 5 Rounds! Game over"
-    }
-    }
-
-function rpsGame(){
-    
-    for (playerScore < winnerScore || computerScore < winnerScore;;){
-
-        if(playerPlay == computerPlay){
-        return "It's a tie!"
-
-        }else if (playerPlay == 'rock' && computerPlay == 'paper'
-            || playerPlay == 'paper' && computerPlay == 'scissors' 
-            || playerPlay == 'scissors' && computerPlay == 'rock'){
-            return "Computer wins!", computerScore += 1
-
-        }else if (playerPlay == 'rock' && computerPlay == 'scissors' 
-            || playerPlay == 'scissors' && computerPlay == 'paper'
-            || playerPlay == 'paper' && computerPlay == 'rock'){
-            return "You Win!", playerScore +=1
-        }
-        console.log('player score = ' + {playerScore})
-        console.log('Computer score = ' + {computerScore})
-    }
 }
-
-rpsGame()
-
 
