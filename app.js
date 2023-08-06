157fbe9e192f7fd82359b949626a9e5f45e57705
dillonpw <dillonpwalsh10@gmail.com>
2023-08-06 18:15:15 -0400

    formatting changes

--- app.js
+++ app.js
@@ -4,76 +4,77 @@ let ties = 0;
 const maxWins = 5;
 
 function computerChoice() {
-  const choices = ['rock', 'paper', 'scissors'];
-  const randomNum = Math.floor(Math.random() * choices.length);
-  return choices[randomNum];
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNum = Math.floor(Math.random() * choices.length);
+    return choices[randomNum];
 }
 
 function updateScore(userChoice, computerChoiceValue, result) {
-  document.getElementById('userScore').textContent = userWins;
-  document.getElementById('computerScore').textContent = computerWins;
-  document.getElementById('ties').textContent = ties
- 
-  const battleText = `You chose: ${userChoice}\nComputer chose: ${computerChoiceValue}\n\n${result}`;
-  document.getElementById('battle').textContent = battleText;
- 
+    document.getElementById('userScore').textContent = userWins;
+    document.getElementById('computerScore').textContent = computerWins;
+    document.getElementById('ties').textContent = ties;
 
+    const battleText = `You chose: ${userChoice}\nComputer chose: ${computerChoiceValue}\n\n${result}`;
+    document.getElementById('battle').textContent = battleText;
 
-  if (userWins === maxWins) {
-    const customAlert = document.getElementById('customAlert');
-  const customAlertMessage = document.getElementById('customAlertMessage');
-  const customAlertButton = document.getElementById('customAlertButton');
-  
-  customAlertMessage.textContent = "You Won! Click here to play again.";
-  customAlert.style.display = 'flex';
-  
-  customAlertButton.addEventListener('click', function() {
-    customAlert.style.display = 'none';
-  });
-    resetGame();
-  } else if (computerWins === maxWins) {
-    const customAlert = document.getElementById('customAlert');
-  const customAlertMessage = document.getElementById('customAlertMessage');
-  const customAlertButton = document.getElementById('customAlertButton');
-  
-  customAlertMessage.textContent = "Computer Won! Click here to play again.";
-  customAlert.style.display = 'flex';
-  
-  customAlertButton.addEventListener('click', function() {
-    customAlert.style.display = 'none';
-  });
-    resetGame();
-  }
+    if (userWins === maxWins) {
+        const customAlert = document.getElementById('customAlert');
+        const customAlertMessage =
+            document.getElementById('customAlertMessage');
+        const customAlertButton = document.getElementById('customAlertButton');
+
+        customAlertMessage.textContent = 'You Won! Click here to play again.';
+        customAlert.style.display = 'flex';
+
+        customAlertButton.addEventListener('click', function () {
+            customAlert.style.display = 'none';
+        });
+        resetGame();
+    } else if (computerWins === maxWins) {
+        const customAlert = document.getElementById('customAlert');
+        const customAlertMessage =
+            document.getElementById('customAlertMessage');
+        const customAlertButton = document.getElementById('customAlertButton');
+
+        customAlertMessage.textContent =
+            'Computer Won! Click here to play again.';
+        customAlert.style.display = 'flex';
+
+        customAlertButton.addEventListener('click', function () {
+            customAlert.style.display = 'none';
+        });
+        resetGame();
+    }
 }
 
 function resetGame() {
-  userWins = 0;
-  computerWins = 0;
-  ties = 0;
-  updateScore('', '', '');
-  
-  // Hide battle text
-  document.getElementById('battle').style.display = 'none';
+    userWins = 0;
+    computerWins = 0;
+    ties = 0;
+    updateScore('', '', '');
+
+    // Hide battle text
+    document.getElementById('battle').style.display = 'none';
 }
 
 function play(userChoice) {
-  const computerChoiceValue = computerChoice();
-  let result = '';
+    const computerChoiceValue = computerChoice();
+    let result = '';
 
-  if (userChoice === computerChoiceValue) {
-    result = "It's a tie!";
-    ties++;
-  } else if (
-    (userChoice === 'rock' && computerChoiceValue === 'scissors') ||
-    (userChoice === 'paper' && computerChoiceValue === 'rock') ||
-    (userChoice === 'scissors' && computerChoiceValue === 'paper')
-  ) {
-    result = 'You win!';
-    userWins++;
-  } else {
-    result = 'Computer wins!';
-    computerWins++;
-  }
+    if (userChoice === computerChoiceValue) {
+        result = "It's a tie!";
+        ties++;
+    } else if (
+        (userChoice === 'rock' && computerChoiceValue === 'scissors') ||
+        (userChoice === 'paper' && computerChoiceValue === 'rock') ||
+        (userChoice === 'scissors' && computerChoiceValue === 'paper')
+    ) {
+        result = 'You win!';
+        userWins++;
+    } else {
+        result = 'Computer wins!';
+        computerWins++;
+    }
 
-  updateScore(userChoice, computerChoiceValue, result);
+    updateScore(userChoice, computerChoiceValue, result);
 }
